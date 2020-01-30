278ed080d92efe7772562d522a8adbc9dca9e258
Krishna Vikas <venkatakrishnavika.b@husky.neu.edu>
2020-01-30 16:24:50 -0500

    first commit

---
+++ Assignment1/Assignment1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment1
+//
+//  Created by Krishna Vikas on 1/30/20.
+//  Copyright © 2020 Krishna Vikas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
